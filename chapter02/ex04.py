b4b29061eb4f8b4c6595115f26e279815e8a7ef7
kimsungkwang <skk405@naver.com>
2022-01-29 12:43:49 +0900

    변수

---
+++ chapter02/ex04.py
@@ -0,0 +1,8 @@
+age = input('몇 살이세요? ')
+print(age)
+
+price = input('가격을 입력하세요 : ')
+num = input('수량을 입력하세요 : ')
+sum = int(price) * int(num)
+
+print('총액은', sum, '입니다.')
---
+++ chapter02/ex05.py
@@ -0,0 +1,10 @@
+from re import A
+
+
+score = 98
+print(score)
+score = 55
+print(score)
+
+a = 1234
+print(type(a))
