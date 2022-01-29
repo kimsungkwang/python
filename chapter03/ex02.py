196ed597a032147456d261cd2782bc6dc2c8631f
kimsungkwang <skk405@naver.com>
2022-01-29 12:54:55 +0900

    타입

---
+++ chapter03/ex02.py
@@ -0,0 +1,14 @@
+a = 0x1a
+print(a)
+
+
+print(ord('a'))
+print(chr(98))
+
+member = ['dog', 'cat', 'pig', 'cow']
+print(type(member))
+print(member)
+
+member = ('dog', 'cat', 'pig', 'cow')
+print(type(member))
+print(member)
