068fa15579e537e3329ed6de56ce59bc3cef1f92
kimsungkwang <skk405@naver.com>
2022-01-30 17:45:38 +0900

    반복문

---
+++ chapter06/ex08.py
@@ -0,0 +1,10 @@
+# 구구단 
+
+for dan in range (2, 10):
+    print(dan,"단")
+
+    for hang in range(2,10):
+        
+        print(dan,"*", hang, "=", dan*hang)
+
+    print()
---
+++ chapter06/ex09.py
@@ -0,0 +1,5 @@
+for y in range(1, 10):
+    for x in range(y):
+        print("*", end='')
+
+    print()
